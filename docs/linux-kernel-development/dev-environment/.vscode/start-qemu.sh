2a52525bdf9923683ce73d170051ac743ebe084b
wg21908 <72874897+wg21908@users.noreply.github.com>
2025-10-22 11:52:07 -0400

    Update start-qemu.sh

--- docs/linux-kernel-development/dev-environment/.vscode/start-qemu.sh
+++ docs/linux-kernel-development/dev-environment/.vscode/start-qemu.sh
@@ -12,7 +12,7 @@ sudo chown secra:secra /home/secra/git/linux/arch/x86/boot/bzImage
 
 qemu-system-x86_64 \
   -kernel /home/secra/git/linux/arch/x86/boot/bzImage \
-  -initrd /home/secra/initramfs.cpio.gz \
+  -initrd /home/secra/git/linux/initramfs.cpio.gz \
   -append "console=ttyS0 rdinit=/init" \
   -serial mon:stdio -s -S \
   -nographic
