3861d10f6d48dc7768a587e79169b58d6f6d386e
wg21908 <72874897+wg21908@users.noreply.github.com>
2025-10-22 11:52:29 -0400

    Update create-initramfs-archive.sh

--- docs/linux-kernel-development/dev-environment/create-initramfs-archive.sh
+++ docs/linux-kernel-development/dev-environment/create-initramfs-archive.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-# executed as secra
-
 cd ~/rootfs
-find . -print0 | cpio --null -ov --format=newc | gzip -9 > ../initramfs.cpio.gz
+find . -print0 | cpio --null -ov --format=newc | gzip -9 > /home/secra/git/linux/initramfs.cpio.gz
